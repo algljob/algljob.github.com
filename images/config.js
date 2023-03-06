4fd6fb1878e537b8094afff52909dd0fc0cab54a
algljob <60602181+algljob@users.noreply.github.com>
2023-03-06 12:47:47 -0500

    Add files via upload

---
+++ images/config.js
@@ -0,0 +1,55 @@
+product_config = {
+    // Main page image scroller
+    "scroller": [
+        {"image": "_scroller_/1.png", "text": "CARTERAS"},
+        {"image": "_scroller_/1.png", "text": "ZAPATOS"},
+        {"image": "_scroller_/1.png", "text": "BILLETERAS"}
+    ],
+
+    // Product categories
+    "categories": [
+        // Carteras
+        {
+            "name": "Carteras",
+            "products": [
+                {
+                    "images": {
+                        "blue": ["carteras/1/1.jpeg", "carteras/1/2.jpeg", "carteras/1/3.jpeg", "carteras/1/4.jpeg", "carteras/1/5.jpeg", "carteras/1/6.jpeg"]
+                    },
+                    "colors": {
+                        "blue": "Azul"
+                    },
+                    "name": "Bolsa Xenia",
+                    "description": "Una bolsa bonita para ti para cada dia",
+                    "price": 120,
+                    "code": "HJFK4"
+                }
+            ]
+        },
+
+        // Billeteras
+        {
+            "name": "Billeteras",
+            "products": [
+                {
+                    "images": {
+                        "blue": ["billeteras/1/1.jpeg", "billeteras/1/2.jpeg", "billeteras/1/3.jpeg", "billeteras/1/4.jpeg"],
+                        "maroon": ["billeteras/1/5.jpeg"],
+                        "red": ["billeteras/1/6.jpeg"],
+                        "yellow": ["billeteras/1/7.jpeg"]
+                    },
+                    "colors": {
+                        "blue": "Azul",
+                        "maroon": "Marron",
+                        "red": "Rojo",
+                        "yellow": "Amarillo"
+                    },
+                    "name": "Billetera Bonita",
+                    "description": "Para tu alcancio",
+                    "price": 80,
+                    "code": "HAFK4"
+                }
+            ]
+        }
+    ]
+}
